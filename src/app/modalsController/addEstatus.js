03835b857631120d2b60759a7d5b3dcd3436f8cd
Ariaklirj <lir753j@gmail.com>
2018-02-27 22:49:40 -0600

    doble decision

--- src/app/modalsController/addEstatus.js
+++ src/app/modalsController/addEstatus.js
@@ -27,13 +27,16 @@
             vm.capitulos;
             vm.estatus;
             vm.name
+            vm.disableEstatusSelect2=true;
+            vm.disableDecisionSelect2=true;
+            vm.showSpanDecisionDoble=false;
             vm.capitulo_seleccionado = null;
             vm.estatus_seleccionado = null;
             vm.disableEstatusSelect = true;
             vm.disableEstatusFinal = true;
             vm.disableSpanCapitulos = false;
             vm.decision_seleccionada = null;
-            vm.SpanDecisionNoSeleccionado=false;
+            vm.SpanDecisionNoSeleccionado = false;
             vm.spanTitle = false;
             vm.SpanEstatusNoSeleccionado = false;
             vm.disableSpanEstatus = false;
@@ -41,7 +44,10 @@
             vm.primerEstatus = false;
             vm.saveDisable = false;
             vm.decisions = [];
+            vm.decisions2 = [];
             vm.EstatusFinal = false;
+            vm.disableMasDeUnaDecision = false;
+            vm.dobleDecisionAnterior = false;
 
             Auth.ObtenerCapitulos(criteria)
                 .then(function (respuesta) {
@@ -75,6 +81,19 @@
                 console.log(vm.primerEstatus)
             }
 
+            vm.cambioDobleDecisionAnterior = function () {
+                if (!vm.dobleDecisionAnterior) {
+                    vm.disableEstatusSelect2=true;
+                    vm.disableDecisionSelect2=true;
+                    vm.dobleDecisionAnterior = false;
+                }
+                else {
+                    vm.disableEstatusSelect2=false;
+                    vm.disableDecisionSelect2=false;
+                    vm.dobleDecisionAnterior = true;
+                }
+                console.log(vm.dobleDecisionAnterior)
+            }
             vm.cambioEstatusFinal = function () {
                 if (!vm.EstatusFinal) {
                     vm.EstatusFinal = false;
@@ -97,6 +116,7 @@
             }
 
             vm.cambioEstatus = function () {
+                vm.decisions = [];
                 vm.SpanEstatusNoSeleccionado = false;
                 vm.disableSpanDecision = false;
                 vm.saveDisable = false;
@@ -170,6 +190,85 @@
 
             }
 
+
+            vm.cambioEstatus2 = function () {
+                vm.SpanEstatusNoSeleccionado2 = false;
+                vm.disableSpanDecision2 = false;
+                vm.saveDisable = false;
+                vm.decisions2 = [];
+                var criteria = {
+                    status: vm.estatus[vm.estatus_seleccionado2].id_estatus
+                }
+                Auth.ObtenerDecisiones(criteria)
+                    .then(function (response) {
+                        var i = 0;
+                        if (response.data[0].idNextStatusOne == null || response.data[0].idNextStatusTwo == null || response.data[0].idNextStatusThree == null) {
+                            if (response.data[0].idNextStatusOne == null) {
+                                var objDecision1 = {
+                                    index: i,
+                                    name: response.data[0].descriptionOne,
+                                    id_decision: {
+                                        id_decision: response.data[0].id_decision
+                                    },
+                                    updateDecision: {
+                                        idNextStatusOne: ""
+                                    },
+                                    type: 1
+                                }
+                                vm.decisions2.push(objDecision1);
+                                i++;
+                            }
+                            if (response.data[0].idNextStatusTwo == null) {
+                                var objDecision2 = {
+                                    index: i,
+                                    name: response.data[0].descriptionTwo,
+                                    id_decision: {
+                                        id_decision: response.data[0].id_decision
+                                    },
+                                    updateDecision: {
+                                        idNextStatusTwo: ""
+                                    },
+                                    type: 2
+
+                                }
+                                vm.decisions2.push(objDecision2);
+                                i++
+                            }
+
+                            if (!response.data[0].singleDecision && response.data[0].idNextStatusThree == null) {
+                                var objDecision3 = {
+                                    index: i,
+                                    name: response.data[0].descriptionThree,
+                                    id_decision: {
+                                        id_decision: response.data[0].id_decision
+                                    },
+                                    updateDecision: {
+                                        idNextStatusThree: ""
+                                    },
+                                    type: 3
+
+                                }
+                                vm.decisions2.push(objDecision3);
+                                i++;
+                            }
+                        } else {
+                            vm.disableSpanDecision2 = true;
+                            vm.saveDisable = true;
+                        }
+
+
+
+                    })
+                    .catch(function (err) {
+                        vm.saveDisable = true;
+                        vm.disableSpanDecision2 = true;
+                    })
+
+            }
+
+
+
+
             vm.cambioCapitulo = function () {
                 if (vm.capitulo_seleccionado != null) {
                     vm.disableSpanEstatus = false;
@@ -209,7 +308,7 @@
                 }
             }
             vm.ok = function () {
-
+                vm.showSpanDecisionDoble=false;
                 if (vm.primerEstatus) {
 
                     if (vm.title != "") {
@@ -235,67 +334,166 @@
                         vm.spanTitle = true;
                     }
                 } else {
-                    if (vm.title != "") {
-                        var newEstatus = {}
-                        if (vm.estatus_seleccionado != null) {
-                            newEstatus = {
-                                title: vm.name,
+                    if (vm.dobleDecisionAnterior) {
+                        console.log("doble");
+                        if (vm.title != "") {
+                            var newEstatus = {}
+                            if (vm.estatus_seleccionado != null) {
+                                if (vm.estatus_seleccionado2 != null) {
+                                    if (vm.estatus[vm.estatus_seleccionado].id_estatus ==vm.estatus[vm.estatus_seleccionado2].id_estatus) {
+                                        vm.showSpanDecisionDoble=true;
+                                    } else {
+                                        newEstatus = {
+                                            title: vm.name,
 
-                                isStart: vm.primerEstatus,
-                                isEnd: vm.EstatusFinal,
-                                lastStatus: vm.estatus[vm.estatus_seleccionado].id_estatus,
-                                admin: $localStorage.idAdmin,
-                                chapter: vm.capitulos[vm.capitulo_seleccionado].id_chapter
+                                            isStart: vm.primerEstatus,
+                                            isEnd: vm.EstatusFinal,
+                                            lastStatus: vm.estatus[vm.estatus_seleccionado].id_estatus,
+                                            lastStatus2: vm.estatus[vm.estatus_seleccionado2].id_estatus,
+                                            admin: $localStorage.idAdmin,
+                                            chapter: vm.capitulos[vm.capitulo_seleccionado].id_chapter
 
-                            };
-                            console.log(newEstatus);
-                            if (vm.decision_seleccionada != null) {
-                                Auth.CrearEstatus(newEstatus)
-                                    .then(function (response) {
+                                        };
+                                        console.log(newEstatus);
+                                        if (vm.decision_seleccionada != null) {
+                                            if (vm.decision_seleccionada2 != null) {
+                                                Auth.CrearEstatus(newEstatus)
+                                                    .then(function (response) {
 
-                                        switch (vm.decisions[vm.decision_seleccionada].type) {
-                                            case 1:
-                                                vm.decisions[vm.decision_seleccionada].updateDecision.idNextStatusOne = response.data.id_estatus;
-                                                break;
+                                                        switch (vm.decisions[vm.decision_seleccionada].type) {
+                                                            case 1:
+                                                                vm.decisions[vm.decision_seleccionada].updateDecision.idNextStatusOne = response.data.id_estatus;
+                                                                break;
 
-                                            case 2:
-                                                vm.decisions[vm.decision_seleccionada].updateDecision.idNextStatusTwo = response.data.id_estatus;
-                                                break;
+                                                            case 2:
+                                                                vm.decisions[vm.decision_seleccionada].updateDecision.idNextStatusTwo = response.data.id_estatus;
+                                                                break;
 
-                                            case 3:
-                                                vm.decisions[vm.decision_seleccionada].updateDecision.idNextStatusThree = response.data.id_estatus;
-                                                break;
+                                                            case 3:
+                                                                vm.decisions[vm.decision_seleccionada].updateDecision.idNextStatusThree = response.data.id_estatus;
+                                                                break;
+                                                        }
+                                                        switch (vm.decisions2[vm.decision_seleccionada2].type) {
+                                                            case 1:
+                                                                vm.decisions2[vm.decision_seleccionada2].updateDecision.idNextStatusOne = response.data.id_estatus;
+                                                                break;
+
+                                                            case 2:
+                                                                vm.decisions2[vm.decision_seleccionada2].updateDecision.idNextStatusTwo = response.data.id_estatus;
+                                                                break;
+
+                                                            case 3:
+                                                                vm.decisions2[vm.decision_seleccionada2].updateDecision.idNextStatusThree = response.data.id_estatus;
+                                                                break;
+                                                        }
+                                                        var updateDecisionReq2 = {
+                                                            id_decision: vm.decisions2[vm.decision_seleccionada2].id_decision,
+                                                            updateDecision: vm.decisions2[vm.decision_seleccionada2].updateDecision
+                                                        }
+                                                        var updateDecisionReq = {
+                                                            id_decision: vm.decisions[vm.decision_seleccionada].id_decision,
+                                                            updateDecision: vm.decisions[vm.decision_seleccionada].updateDecision
+                                                        }
+                                                        Auth.ActualizarDecisiones(updateDecisionReq)
+                                                            .then(function (responseUpdate) {
+                                                                console.log(responseUpdate);
+                                                                Auth.ActualizarDecisiones(updateDecisionReq2)
+                                                                    .then(function (responseUpdate) {
+                                                                        console.log(responseUpdate);
+                                                                        $rootScope.$emit('statusAdded');
+                                                                        $uibModalInstance.dismiss('close');
+                                                                    })
+                                                                    .catch(function (err) {
+                                                                    })
+                                                            })
+                                                            .catch(function (err) {
+
+                                                            })
+                                                    })
+                                                    .catch(function (err) {
+                                                        console.log(err);
+                                                    })
+                                            } else {
+                                                vm.SpanDecisionNoSeleccionado2 = true;
+                                            }
+                                        } else {
+                                            vm.SpanDecisionNoSeleccionado = true;
                                         }
+                                    }
 
 
-                                        var updateDecisionReq = {
-                                            id_decision: vm.decisions[vm.decision_seleccionada].id_decision,
-                                            updateDecision: vm.decisions[vm.decision_seleccionada].updateDecision
-                                        }
-                                        Auth.ActualizarDecisiones(updateDecisionReq)
-                                            .then(function (responseUpdate) {
-                                                console.log(responseUpdate);
-                                                $rootScope.$emit('statusAdded');
-                                                $uibModalInstance.dismiss('close');
-                                            })
-                                            .catch(function (err) {
-
-                                            })
-                                    })
-                                    .catch(function (err) {
-                                        console.log(err);
-                                    })
+                                } else {
+                                    vm.SpanEstatusNoSeleccionado2 = true;
+                                }
                             } else {
-                                vm.SpanDecisionNoSeleccionado=true;
+                                vm.SpanEstatusNoSeleccionado = true;
                             }
-
                         } else {
-                            vm.SpanEstatusNoSeleccionado = true;
+                            vm.spanTitle = false;
                         }
                     } else {
-                        vm.spanTitle = false;
-                    }
+                        if (vm.title != "") {
+                            var newEstatus = {}
+                            if (vm.estatus_seleccionado != null) {
+                                newEstatus = {
+                                    title: vm.name,
 
+                                    isStart: vm.primerEstatus,
+                                    isEnd: vm.EstatusFinal,
+                                    lastStatus: vm.estatus[vm.estatus_seleccionado].id_estatus,
+                                    admin: $localStorage.idAdmin,
+                                    chapter: vm.capitulos[vm.capitulo_seleccionado].id_chapter
+
+                                };
+                                console.log(newEstatus);
+                                if (vm.decision_seleccionada != null) {
+                                    Auth.CrearEstatus(newEstatus)
+                                        .then(function (response) {
+
+                                            switch (vm.decisions[vm.decision_seleccionada].type) {
+                                                case 1:
+                                                    vm.decisions[vm.decision_seleccionada].updateDecision.idNextStatusOne = response.data.id_estatus;
+                                                    break;
+
+                                                case 2:
+                                                    vm.decisions[vm.decision_seleccionada].updateDecision.idNextStatusTwo = response.data.id_estatus;
+                                                    break;
+
+                                                case 3:
+                                                    vm.decisions[vm.decision_seleccionada].updateDecision.idNextStatusThree = response.data.id_estatus;
+                                                    break;
+                                            }
+
+
+                                            var updateDecisionReq = {
+                                                id_decision: vm.decisions[vm.decision_seleccionada].id_decision,
+                                                updateDecision: vm.decisions[vm.decision_seleccionada].updateDecision
+                                            }
+                                            Auth.ActualizarDecisiones(updateDecisionReq)
+                                                .then(function (responseUpdate) {
+                                                    console.log(responseUpdate);
+                                                    $rootScope.$emit('statusAdded');
+                                                    $uibModalInstance.dismiss('close');
+                                                })
+                                                .catch(function (err) {
+
+                                                })
+                                        })
+                                        .catch(function (err) {
+                                            console.log(err);
+                                        })
+                                } else {
+                                    vm.SpanDecisionNoSeleccionado = true;
+                                }
+
+                            } else {
+                                vm.SpanEstatusNoSeleccionado = true;
+                            }
+                        } else {
+                            vm.spanTitle = false;
+                        }
+
+                    }
                 }
             }
             vm.cancel = function () {
